d34641534a69226fd96dc3c0917df406a94c890c
sales_taro <zituyuu777@gmail.com>
2023-09-25 07:34:29 +0900

    C1

---
+++ customer.py
@@ -0,0 +1,14 @@
+'''
+ken = Customer(first_name="Ken", family_name="Tanaka")
+ken.full_name()  # "Ken Tanaka" という値を返す
+
+tom.full_name()  # "Tom Ford" という値を返す
+'''
+class Customer:
+    def __init__(self,first_name,family_name):
+        self.first_name=first_name.capitalize()
+        self.family_name=family_name.capitalize()
+        print(self.first_name,self.family_name)
+
+ken = Customer(first_name="Ken",family_name="Tanaka")
+tom = Customer(first_name="Tom",family_name="Ford")
