d48ec8f8fa1b896b2e7c81918995adb5bef45f40
sales_taro <zituyuu777@gmail.com>
2023-09-25 07:38:00 +0900

    C1-revised

--- customer.py
+++ customer.py
@@ -10,5 +10,5 @@ class Customer:
         self.family_name=family_name.capitalize()
         print(self.first_name,self.family_name)
 
-ken = Customer(first_name="Ken",family_name="Tanaka")
-tom = Customer(first_name="Tom",family_name="Ford")
+ken = Customer(first_name="ken",family_name="tanaka")
+tom = Customer(first_name="tom",family_name="ford")
